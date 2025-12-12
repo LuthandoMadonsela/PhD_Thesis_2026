0fc8dd79c9ca0d23dcc8ac9dea30dd4e857adfd3
Luthando Madonsela <91069314+LuthandoMadonsela@users.noreply.github.com>
2025-12-12 10:20:13 +0200

    Create raxml.sh

---
+++ PhyloPhlAn and RAxML/raxml.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#SBATCH --partition=agrp
+#SBATCH --time=14-00:00:00
+#SBATCH -N 1 # Nodes
+#SBATCH -n 1 # Tasks
+#SBATCH --cpus-per-task=8
+#SBATCH --mem 50G
+#SBATCH --error=raxml.%J.err
+#SBATCH --output=raxml.%J.out
+
+cd /home/luthando/Phylophlan/Phylophlan_output
+
+eval "$(conda shell.bash hook)"
+conda activate phylophlan
+
+raxmlHPC-PTHREADS-SSE3 -s your_sample_concatenated.aln -n name_of_tree_here.nwk -f a -m PROTGAMMAAUTO -N 100 -p 42 -x 42 -T 8 ###default is -N 1000, but I used 100 bootstraps for low diversity
+
+conda deactivate
